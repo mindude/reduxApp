b1dc23639165e08f5eabc129b57994c136206ad4
MindLabs <mindlabs.arkusnexus@gmail.com>
2018-07-12 17:45:21 -0700

    Let's start installing express generator

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@
 import React from 'react';
 import {render} from 'react-dom';
 import {Provider} from 'react-redux';
+// REACT-ROUTER
+import {Router, Route, IndexRoute, browserHistory} from 'react-router';
 
 import {applyMiddleware, createStore} from 'redux';
 
@@ -16,16 +18,22 @@ import {postBooks, deleteBooks, updateBooks} from './actions/booksActions';
 const store = createStore(reducers);
 
 import BooksList from './components/pages/booksList';
-import Menu from './components/menu';
-import Footer from './components/footer';
+import Cart from './components/pages/cart';
+import BooksForm from './components/pages/booksForm';
+import Main from './main';
+
+const Routes = (
+	<Provider store={store}>
+		<Router history={browserHistory}>
+			<Route path="/" component={Main}>
+				<IndexRoute component={BooksList} />
+				<Route path="/admin" component={BooksForm} />
+				<Route path="/cart" component={Cart} />
+			</Route>
+		</Router>
+	</Provider>
+)
 
 render(
-	<Provider store={store}>
-	<div>
-		<Menu />
-		<BooksList />
-		<Footer />
-	</div>
-
-	</Provider>, document.getElementById('app')
+	Routes, document.getElementById('app')
 );
